174ec73a8b3d38bc4beda72e4993e0624d75a74b
Kyle Graham <kylegraham05@gmail.com>
2024-04-16 13:51:38 -0700

    Format template files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <>
-      <h1 className='text-3xl font-semibold underline'>Hello World!</h1>
-    </>
-  )
+    return (
+        <>
+            <h1 className='text-3xl font-semibold underline'>Hello World!</h1>
+        </>
+    );
 }
 
-export default App
+export default App;
